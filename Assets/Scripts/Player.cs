6e29bc14b97c7cdedfb13c8c158105ca76d6619a
khaledrizk <44033157+khaledrizk@users.noreply.github.com>
2018-11-07 19:57:15 +0200

    Player Action

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour {
     // members or variables
     public float moveSpeed = 7.0f;
 
+    public GameObject bulletSpawner;
+    public GameObject bullet;
+
     // methods or functions
 
 	
@@ -16,6 +19,27 @@ public class Player : MonoBehaviour {
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            //transform.GetChild(0).lookAt();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
         }
 	}
+    void Shoot() {
+        Instantiate(bullet.transform, bulletSpawner.transform.position, bulletSpawner.transform.rotation);
+    }
 }
